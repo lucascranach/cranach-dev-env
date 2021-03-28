a53ad266822a0dd40fe6ad36653b9c836a799b9f
Jorge H. F. Pereira <j.pereira@klickmeister.de>
2021-03-28 23:59:04 +0200

    feat(filter): add new route for filter actions

--- search/artefact-server/index.js
+++ search/artefact-server/index.js
@@ -42,22 +42,6 @@ const paintingItems = paintingFilenames.reduce(compileItems, []);
 const archivalItems = archivalFilenames.reduce(compileItems, []);
 
 
-const graphics = {
-	'de': graphicItems.filter(item => item.metadata.langCode === 'de' && item.images),
-	'en': graphicItems.filter(item => item.metadata.langCode === 'en' && item.images),
-};
-
-const paintings = {
-	'de': paintingItems.filter(item => item.metadata.langCode === 'de' && item.images),
-	'en': paintingItems.filter(item => item.metadata.langCode === 'en' && item.images),
-};
-
-const archivals = {
-	'de': archivalItems.filter(item => item.metadata.langCode === 'de'),
-	'en': archivalItems.filter(item => item.metadata.langCode === 'en'),
-};
-
-
 const MAX_RESULTS = 100;
 
 
@@ -68,9 +52,9 @@ app.get('/random', (req, res) => {
 	const paintingsCnt = Math.ceil(Math.random() * (MAX_RESULTS - graphicsCnt));
 	const archivalsCnt = MAX_RESULTS - (graphicsCnt + paintingsCnt);
 
-	const langGraphicItems = graphics[lang] ? graphics[lang] : graphics['en'];
-	const langPaintingItems = paintings[lang] ? paintings[lang] : paintings['en'];
-	const langArchivalItems = archivals[lang] ? archivals[lang] : archivals['en'];
+	const langGraphicItems = graphicItems.filter(item => item.metadata.langCode === lang);;
+	const langPaintingItems = paintingItems.filter(item => item.metadata.langCode === lang);
+	const langArchivalItems = archivalItems.filter(item => item.metadata.langCode === lang);
 
 	const randomizedGraphicItems = langGraphicItems.sort(() => Math.random() - 0.5);
 	const randomizedPaintingItems = langPaintingItems.sort(() => Math.random() - 0.5);
@@ -85,9 +69,118 @@ app.get('/random', (req, res) => {
 
 	res.statusCode = 200;
 	res.json(foundItems);
-})
+});
 
 
+app.get('/', (req, res) => {
+	const params = Object.entries(req.query).reduce((acc, [key, value]) => {
+		const [cleanKey, filterOp = ''] = key.split(':');
+
+		const type = !!filterOp ? 'filter' : 'remaining';
+
+		acc[type][cleanKey] = {
+			value,
+			op: filterOp,
+		};
+
+		return acc;
+	}, { filter: {}, remaining: {} });
+
+	const { lang = { value: 'de' } } = params.remaining;
+
+	const langGraphicItems = graphicItems.filter(item => item.metadata.langCode === lang.value);
+	const langPaintingItems = paintingItems.filter(item => item.metadata.langCode === lang.value);
+	const langArchivalItems = archivalItems.filter(item => item.metadata.langCode === lang.value);
+
+	const allItems = [...langGraphicItems, ...langPaintingItems, ...langArchivalItems];
+
+	let filteredItems = Object.entries(params.filter).reduce((acc, [key, data]) => {
+		if (['dating_begin', 'dating_end'].includes(key)) {
+			const fieldName = key === 'dating_begin' ? 'begin' : 'end';
+			const val = parseInt(data.value, 10);
+
+			if (Number.isNaN(val)) {
+				return acc;
+			}
+
+			switch(data.op) {
+				case 'lt':
+					return acc.filter((item) => {
+						if (!item.dating[fieldName]) {
+							return true;
+						}
+						return item.dating[fieldName] < val;
+					});
+					break;
+
+				case 'lte':
+					return acc.filter((item) => {
+						if (!item.dating[fieldName]) {
+							return true;
+						}
+						return item.dating[fieldName] <= val;
+					});
+					break;
+
+				case 'gt':
+					return acc.filter((item) => {
+						if (!item.dating[fieldName]) {
+							return true;
+						}
+						return item.dating[fieldName] > val;
+					});
+					break;
+
+				case 'gte':
+					return acc.filter((item) => {
+						if (!item.dating[fieldName]) {
+							return true;
+						}
+						return item.dating[fieldName] >= val;
+					});
+					break;
+			}
+		}
+
+		if (key === 'term') {
+			switch(data.op) {
+				case 'eq':
+					const termRegExp = new RegExp(`${data.value}`, 'ig');
+					return acc.filter((item) => termRegExp.exec(item.metadata.title));
+					break;
+			}
+		}
+
+		return acc;
+	}, allItems);
+
+	if (params.remaining['from']) {
+		const fromNumber = parseInt(params.remaining['from'].value, 10);
+
+		if (!Number.isNaN(fromNumber)) {
+			filteredItems = filteredItems.slice(fromNumber);
+		}
+	}
+
+	if (params.remaining['size']) {
+		const sizeNumber = parseInt(params.remaining['size'].value, 10);
+
+		if (!Number.isNaN(sizeNumber)) {
+			filteredItems = filteredItems.slice(0, sizeNumber);
+		} else {
+		}
+	}
+
+	const filteredItemsMetadata = filteredItems.map((item) => item.metadata);
+
+	const results = {
+		graphics: filteredItemsMetadata.filter((item) => item.entityType === 'GRAPHIC'),
+		paintings: filteredItemsMetadata.filter((item) => item.entityType === 'PAINTING'),
+		archivals: filteredItemsMetadata.filter((item) => item.entityType === 'ARCHIVAL'),
+	}
+
+	res.json(results);
+});
 
 
 app.listen(port, () => {
